6e1b8a6a9ca98b1863c1775b36268c3766e6b95f
Joselyn  Andreina Borges Peña <joselyn.borges27@gmail.com>
2012-09-01 19:17:30 -0430

    Creacion del Modelo del Estudiante

---
+++ application/models/Estudiante.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Estudiante extends CI_Model {
+
+    
+
+}
+
+/* End of file estudiante.php */
+/* Location: ./application/controllers/estudiante.php */
